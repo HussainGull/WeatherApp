771cc5acb2489386a0049c43d709a305b5784b86
Hussain Gull <hussaingulltullah@gmail.com>
2024-11-09 20:06:08 +0500

    Finalized

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let displayResult = document.querySelector('.result-sec-info');
 
 
 let address;
-let weatherData;
+
 inputBox.addEventListener("keydown", (e) => {
 
     if (e.key === 'Enter') {
@@ -36,34 +36,45 @@ inputBox.addEventListener("keydown", (e) => {
     })
 });
 
+function displayWeatherData() {
+    let weatherDataParsed = JSON.parse(localStorage.getItem('weatherDataLS'));
+    if (weatherDataParsed) {
+        displayResult.innerHTML = '';
+        Object.values(weatherDataParsed).forEach((value) => {
+            displayResult.innerHTML += value + '<br>';
+        });
+    }
+}
+
+window.addEventListener('load', displayWeatherData);
 
 searchButton.addEventListener('click', (e) => {
+
     const getWeather = async () => {
         const weatherApi = `http://api.weatherapi.com/v1/current.json?key=6c8c1c8203344b6789a140540240811&q=${latitude},${longitude}&aqi=no`;
         const response = await fetch(weatherApi);
         const data = await response.json();
-        weatherData = {
-            time: (`Time & Date: ${data.location.localtime}`),
-            temperature: (`Temperature: ${data.current.temp_c}`),
-            feelsLike: (`Feel's Like: ${data.current.feelslike_c}`),
-            wind: (`Wind: ${data.current.wind_kph}`),
-        };
-        Object.values(weatherData).forEach((value) => {
-            displayResult.innerHTML += (value + '<br>');
-        });
-        searchButton.addEventListener('click', (e) => {
-            displayResult.innerHTML = '';
-        });
 
+        let weatherData = {
+            time: `Time & Date: ${data.location.localtime}`,
+            temperature: `Temperature: ${data.current.temp_c} °C`,
+            feelsLike: `Feels Like: ${data.current.feelslike_c} °C`,
+            wind: `Wind: ${data.current.wind_kph} kph`
+        };
+
+        localStorage.setItem('weatherDataLS', JSON.stringify(weatherData));
+
+        displayResult.innerHTML = '';
+        Object.values(weatherData).forEach((value) => {
+            displayResult.innerHTML += value + '<br>';
+        });
     };
 
     getWeather().then(() => {
-        console.log("Success");
-    })
-        .catch(() => {
-            console.log("Error");
-        })
-
+        console.log("Weather data successfully retrieved and saved.");
+    }).catch(() => {
+        console.log("Error retrieving weather data.");
+    });
 });
 
 
